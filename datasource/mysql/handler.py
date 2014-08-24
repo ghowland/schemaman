46b0c37b6e85917730eac76e69672fef0db8a11a
Geoff Howland <geoff@gmail.com>
2014-08-24 15:09:16 -0700

    Starting MySQL handler code

--- datasource/mysql/handler.py
+++ datasource/mysql/handler.py
@@ -29,22 +29,58 @@ def ExportData():
   pass
 
 
-def ImportData():
-  """Import/load data to this datasource, based on spec, or everything"""
+def ImportData(drop_first=False, transaction=False):
+  """Import/load data to this datasource, based on spec, or everything.
+  
+  Args:
+    drop_first: boolean, optional: If true, all data is dropped/deleted before
+        the import occurs, otherwise it is an update.  Defaults to false to
+        preserve data.
+    transaction: boolean, optional: If true, import is done as a single
+        transaction.  Defaults to False to avoid extra memory and slowness.
+  
+  Returns: None
+  """
   pass
 
 
 def Put():
-  """Put (insert/update) data into this datasource."""
+  """Put (insert/update) data into this datasource.
+  
+  Works as a single transaction.
+  """
   pass
 
 
-def Get():
+def Get(schema, source, data):
   """Get (select single record) from this datasource.
   
   Can be a 'view', combining several lower level 'tables'.
+  
+  Args:
+    schema: dict, ...
+    source: string, ...
+    data: dict, ...
+  
+  Returns: dict, single record key/values
   """
-  pass
+  keys = list(data.keys())
+  if len(keys) != 1:
+    raise Exception('There should be exactly 1 key at the top level of the data dictionary, which is the table name.')
+  
+  table = keys[0]
+  
+  # If we have an 'id' field
+  #TODO(g): Confirm this is the primary key name, not just "id" all the time
+  #TODO(g): Allow multiple fields for primary key, and do the right thing with them
+  if 'id' in data:
+    sql = "SELECT * FROM %s WHERE id = %s" % (table, data['id'])
+    result = Query(sql)
+    
+    return result
+    
+  else:
+    raise Exception('No "id" field in data, other methods of selection not yet implemented...')
 
 
 def Filter():
@@ -56,7 +92,10 @@ def Filter():
 
 
 def Delete():
-  """Delete a single record."""
+  """Delete a single record.
+  
+  NOTE(g): Processes single record deletes directly, sends fitlered deletes to DeleteFilter()
+  """
   pass
 
 
@@ -65,6 +104,10 @@ def DeleteFilter():
   
   Can be a 'view', combining several lower level 'tables', which makes it a
   cascading delete.
+  
+  Works as a single transaction.
+  
+  NOTE(g): This is called by Delete(), and is not invoked from the CLI directly.
   """
   pass
 
---
+++ datasource/mysql/query.py
@@ -0,0 +1,123 @@
+import mysql.connector
+
+class MySQLCursorDict(mysql.connector.cursor.MySQLCursor):
+    def _row_to_python(self, rowdata, desc=None):
+        row = super(MySQLCursorDict, self)._row_to_python(rowdata, desc)
+        if row:
+            return dict(zip(self.column_names, row))
+        return None
+
+
+def Query(sql):
+	conn = mysql.connector.connect(user='root', password='root', host='opsdb', database='opsdb')
+	cursor = conn.cursor(cursor_class=MySQLCursorDict)
+	
+	cursor.execute(sql)
+	
+	if sql.upper().startswith('INSERT'):
+		result = cursor.lastrowid
+		conn.commit()
+	elif sql.upper().startswith('UPDATE') or sql.upper().startswith('DELETE'):
+		conn.commit()
+		result = None
+	elif sql.upper().startswith('SELECT'):
+		result = cursor.fetchall()
+	else:
+		result = None
+	
+	cursor.close()
+	conn.close()
+	
+	return result
+	
+
+def SanitizeSQL(text):
+  if text == None:
+    text = 'NULL'
+  else:
+    try:
+      text = str(text)
+      
+    except UnicodeEncodeError, e:
+      text = str(text.decode('ascii', 'ignore'))
+  
+  return text.replace("'", "''").replace('\\', '\\\\')
+
+
+
+
+def FormatStringOrNullFromDict(data, key, strict=False):
+  """Returns a quoted and escaped string from the data[key] value, or NULL to insert into a field column
+  
+  If strict=True, then an exception is thrown if the key is missing, otherwise it is NULL.
+  """
+  if key not in data and strict:
+    raise Exception('Missing data key: %s' % key)
+  
+  if key in data and data[key] != None:
+    output = "'%s'" % SanitizeSQL(str(data[key]))
+  else:
+    output = 'NULL'
+  
+  return output
+
+
+def FormatIntOrNullFromDict(data, key, strict=False):
+  """Returns a quoted and escaped string from the data[key] value, or NULL to insert into a field column
+  
+  If strict=True, then an exception is thrown if the key is missing, otherwise it is NULL.
+  """
+  if key not in data and strict:
+    raise Exception('Missing data key: %s' % key)
+  
+  if key in data and data[key] != None:
+    output = "%s" % int(data[key])
+  else:
+    output = 'NULL'
+  
+  return output
+
+
+def ConvertListToDict(list_data, key_field, multi_key_separtor='.'):
+  """Returns a dict keyed on key_field of list_data item dict data.  list_data is a list of dicts.
+  
+  key_field can be string (single value) or list (combined value into a key.field.value dict key)
+  """
+  data = {}
+  
+  for item in list_data:
+    # If this is a single key field, then 
+    if type(key_field) != list:
+      data[item[key_field]] = item
+      
+    else:
+      key = multi_key_separator.join(key_field)
+      data[key] = item
+  
+  return data
+
+
+def ConvertListToDictList(list_data, key_field):
+  """Returns a dict of lists of dicts keyed on key_field of list_data item dict data.  list_data is a list of dicts.
+  
+  key_field can be string (single value) or list (combined value into a key.field.value dict key)
+  """
+  data = {}
+  
+  for item in list_data:
+    # If this is a single key field, then 
+    if type(key_field) != list:
+      if item[key_field] not in data:
+        data[item[key_field]] = []
+      
+      data[item[key_field]].append(item)
+      
+    else:
+      key = multi_key_separator.join(key_field)
+      if key not in data:
+        data[key] = []
+      
+      data[key].append(item)
+  
+  return data
+
