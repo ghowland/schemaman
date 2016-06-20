e626f6cf5c8897d554e56df0cdf03c3dd32df10a
Geoff Howland <ghowland@linkedin.com>
2016-06-20 15:59:20 -0700

    Updated

--- datasource/mysql/query.py
+++ datasource/mysql/query.py
@@ -2,6 +2,7 @@
 Datasource: MySQL: Querying
 """
 
+
 import mysql.connector
 
 
@@ -47,80 +48,4 @@ def SanitizeSQL(text):
       text = str(text.decode('ascii', 'ignore'))
   
   return text.replace("'", "''").replace('\\', '\\\\')
-
-
-def FormatStringOrNullFromDict(data, key, strict=False):
-  """Returns a quoted and escaped string from the data[key] value, or NULL to insert into a field column
-  
-  If strict=True, then an exception is thrown if the key is missing, otherwise it is NULL.
-  """
-  if key not in data and strict:
-    raise Exception('Missing data key: %s' % key)
-  
-  if key in data and data[key] != None:
-    output = "'%s'" % SanitizeSQL(str(data[key]))
-  else:
-    output = 'NULL'
-  
-  return output
-
-
-def FormatIntOrNullFromDict(data, key, strict=False):
-  """Returns a quoted and escaped string from the data[key] value, or NULL to insert into a field column
-  
-  If strict=True, then an exception is thrown if the key is missing, otherwise it is NULL.
-  """
-  if key not in data and strict:
-    raise Exception('Missing data key: %s' % key)
-  
-  if key in data and data[key] != None:
-    output = "%s" % int(data[key])
-  else:
-    output = 'NULL'
-  
-  return output
-
-
-def ConvertListToDict(list_data, key_field, multi_key_separtor='.'):
-  """Returns a dict keyed on key_field of list_data item dict data.  list_data is a list of dicts.
-  
-  key_field can be string (single value) or list (combined value into a key.field.value dict key)
-  """
-  data = {}
-  
-  for item in list_data:
-    # If this is a single key field, then 
-    if type(key_field) != list:
-      data[item[key_field]] = item
-      
-    else:
-      key = multi_key_separator.join(key_field)
-      data[key] = item
-  
-  return data
-
-
-def ConvertListToDictList(list_data, key_field):
-  """Returns a dict of lists of dicts keyed on key_field of list_data item dict data.  list_data is a list of dicts.
-  
-  key_field can be string (single value) or list (combined value into a key.field.value dict key)
-  """
-  data = {}
-  
-  for item in list_data:
-    # If this is a single key field, then 
-    if type(key_field) != list:
-      if item[key_field] not in data:
-        data[item[key_field]] = []
-      
-      data[item[key_field]].append(item)
-      
-    else:
-      key = multi_key_separator.join(key_field)
-      if key not in data:
-        data[key] = []
-      
-      data[key].append(item)
-  
-  return data
 
---
+++ datasource/utility/__init__.py
@@ -0,0 +1,5 @@
+"""
+Bring in all our utility modules into this module space.  They are in different modules just to keep them separate.
+"""
+
+from data_format import *
