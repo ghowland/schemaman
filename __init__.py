64b89245965f6ef620dcc58a16541d5dee0abf9b
Geoff Howland <ghowland@linkedin.com>
2016-07-26 16:13:49 -0700

    Adjusting the PIP packaging

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
+# This is the core of the API, so we will leave it as top level module members
+from datasource import *
+
 # Main()
-#import schemaman
+import schemaman
 
 # Includes and internal library
 import action
-import datasource
 import utility
 
